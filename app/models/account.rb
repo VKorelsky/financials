13e81d501c1075cc47b9f64b0d06821149301506
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-04-11 09:49:28 -0400

    validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,8 @@
 class Account < ApplicationRecord
   belongs_to :debtor, class_name: 'User', foreign_key: 'debtor_id'
   belongs_to :creditor, class_name: 'User', foreign_key: 'creditor_id'
+
+  validates :amount,  presence: true, numericality: true
+  validates :debtor,  presence: true
+  validates :creditor, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :debts, class_name: 'Account', foreign_key: 'debtor_id'
   has_many :credits, class_name: 'Account', foreign_key: 'creditor_id'
+
+  validates :name, presence: true
 end
