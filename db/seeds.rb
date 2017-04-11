93f012ac4e92260eb55ebbd2f83ccbe6dde38ffd
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-04-11 10:33:19 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 # Test model 1, John owes 14 to Fred, Fred owes 7 to John --> John owes 7 to Fred.
 
-r = Room.create(name: 'Test Room')
+r = Room.create(name: 'Test Room', password: 'abc')
 
-j = User.create(name: 'John')
-f = User.create(name: 'Fred')
+j = User.create(name: 'John', room: r)
+f = User.create(name: 'Fred', room: r)
 
 Account.create(debtor: j, creditor: f, amount: 14)
 Account.create(debtor: f, creditor: j, amount: 7)
